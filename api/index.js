6b4c164523f2133ba035052eaf7ed7bfbe3c8df8
mostafizbappy <mostafizur15-8845@diu.edu.bd>
2023-03-09 12:41:36 +0600

    api&routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose"
+import authRoute from "./routes/auth.js"
+import usersRoute from "./routes/users.js"
+import hotelsRoute from "./routes/hotels.js"
+import roomsRoute from "./routes/rooms.js"
 const app = express()
 // require('dotenv').config();
 dotenv.config()
@@ -17,9 +21,15 @@ const connect = async () => { //initial connection. if this connection ok then c
 mongoose.connection.on("disconnected",()=>{
     console.log('mongoDB disconnected');
 })
-mongoose.connection.on("connected",()=>{
-    console.log('mongoDB connected');
-})
+// mongoose.connection.on("connected",()=>{
+//     console.log('mongoDB connected');
+// })
+
+//middlewares
+app.use("/api/auth",authRoute);
+app.use("/api/users",usersRoute);
+app.use("/api/hotels",hotelsRoute);
+app.use("/api/romms",roomsRoute);
 
 app.listen(8800, () => {
     connect();
