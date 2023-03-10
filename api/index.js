adfc09c25a1865420aded911caed3bcd1497dd49
mostafizbappy <mostafizur15-8845@diu.edu.bd>
2023-03-10 23:15:31 +0600

    middleware error handling

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ const connect = async () => { //initial connection. if this connection ok then c
         await mongoose.connect(process.env.MONGO); //'mongodb://127.0.0.1:27017/test'
         console.log('connected to MongoDB')
     } catch (error) {
-        throw error //if there is any problem to conncet code will crush here. not will go for further mongoDB.
+        throw error //if there is any problem to conncet, code will crush here. not will go for further mongoDB.
     }
 };
 
@@ -32,6 +32,18 @@ app.use("/api/auth",authRoute);
 app.use("/api/users",usersRoute);
 app.use("/api/hotels",hotelsRoute);
 app.use("/api/romms",roomsRoute);
+
+// Error handleing middleware
+app.use((err,req,res,next) => {
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || "something went worng!!!"
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack
+    });
+})
 
 app.listen(8800, () => {
     connect();
